ffe0e648a480295de49603644d2b12fccfc13831
cyghello <96031304+cyghello@users.noreply.github.com>
2021-12-20 10:46:52 +0800

    Add files via upload

---
+++ system.c
@@ -0,0 +1,17 @@
+#include "stdlib.h"
+void system_com_arg()
+{
+    system ("mode con cols=100 lines=32");
+    system("title fly_ggg");
+}
+/* int main()
+{
+    system_com_arg();
+    //system ("pause");
+    return 0;
+
+} */
+void system_cls ()
+{
+    system ("cls");
+}
