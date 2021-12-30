92597fa226f18021cf684ef552236b069a3322a7
cyghello <96031304+cyghello@users.noreply.github.com>
2021-12-30 20:10:23 +0800

    Update test.c

--- snake_game/test.c
+++ snake_game/test.c
@@ -99,4 +99,74 @@ void GotoXY(int x, int y)
 
 ************************/
 
+struct coordinate
+{
+    int x;
+    int y;
+    time_t* p;
+    char food_direction[20][20];
+};
+typedef struct coordinate t_coordinate, *p_coordinate;
 
+struct snake_state
+{
+    int x;
+    int y;
+    int direction;
+    int length;
+};
+
+typedef struct snake_state t_snake_state, *p_snake_state;
+
+
+void generate_food (p_coordinate pt_food_coordinate, p_snake_state pt_snake_snake)
+{
+    if (pt_food_coordinate->x == pt_snake_snake->x 
+        && pt_food_coordinate->y == pt_snake_snake->y)
+    {
+        pt_snake_snake->length++;
+        time_t random_value;
+        random_value = time(pt_food_coordinate->p);
+        srand(random_value);
+        pt_food_coordinate->x = rand() % 19;
+        pt_food_coordinate->y = rand() % 19;
+        pt_food_coordinate->food_direction[pt_food_coordinate->x][pt_food_coordinate->y] = 'M';
+
+    }
+    //缺少初始位置的食物
+
+
+}
+
+void pre_generate_food (p_coordinate pt_food_coordinate)
+{
+    pt_food_coordinate->x = 0;
+    pt_food_coordinate->y = 0;
+    pt_food_coordinate->food_direction[20][20] = 0;    //初始化二维数组应该做循环，考虑下是否该初始化
+}
+
+void pre_snake_state (p_snake_state pt_snake_snake)
+{
+    pt_snake_snake->x = 0;
+    pt_snake_snake->y = 0;
+    pt_snake_snake->length = 0;
+    pt_snake_snake->direction = 0;
+}
+
+
+t_coordinate food;
+t_snake_state snake_head;
+
+int main ()
+{
+    pre_generate_food (&food);
+    pre_snake_state (&snake_head);
+    generate_food (&food, &snake_head);
+    putchar (food.food_direction[food.x][food.y]);
+    // int i;
+    // srand((int)time(0));
+    // for( i = 0; i < 10; i++ )
+    //     printf( "Random number #%d: %d\n", i, rand() ); 
+    system ("pause");
+    return 0;
+}
