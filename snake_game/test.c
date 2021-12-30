184a02a3740b09682f8775f54d49af513fa5d4e0
cyghello <96031304+cyghello@users.noreply.github.com>
2021-12-30 12:31:07 +0800

    Update test.c

--- snake_game/test.c
+++ snake_game/test.c
@@ -1,16 +1,21 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "Windows.h"
-#include "def_bottom_para.h"
+#include "def_bottom_var.h"
+//#include "pre_para_set.h"
 
-int pretreatment_map_set(p_map_para p_map_para_set)
+
+/*****************map-test1
+void pre_map_para_set (p_map_para p_map_para_set)
 {
-    p_map_para_set->lines = 0;
-    p_map_para_set->cols = 0;
-    p_map_para_set->border[20][20] = 0;
-    system("cls");
-    system ("title letgo");
-    system ("mode con cols=100 lines=30");
+    p_map_para_set->lines = 100;
+    p_map_para_set->cols = 30;
+    p_map_para_set->border[p_map_para_set->lines][p_map_para_set->cols] = 0;
+
+}
+
+void pretreatment_map_set(p_map_para p_map_para_set)
+{
     for (p_map_para_set->lines = 0; 
          p_map_para_set->lines < 20; p_map_para_set->lines++)
     {
@@ -30,11 +35,9 @@ int pretreatment_map_set(p_map_para p_map_para_set)
                 || p_map_para_set->cols == 0 || p_map_para_set->cols == 19)
             {
                 p_map_para_set->border[p_map_para_set->lines][p_map_para_set->cols] = '#';
-                return 1;
             }
             else 
                 p_map_para_set->border[p_map_para_set->lines][p_map_para_set->cols] = ' ';
-                return 0;
         }
 
     }    
@@ -43,8 +46,28 @@ int pretreatment_map_set(p_map_para p_map_para_set)
 t_map_para tp_map_para;
 int main()
 {   
-    int ret = pretreatment_map_set(&tp_map_para);
-    putchar (tp_map_para.border[20][20]);
+    system("cls");
+    system ("title letgo");
+    system ("mode con cols=100 lines=30");
+    pre_map_para_set (&tp_map_para);
+    pretreatment_map_set (&tp_map_para);
+    for (tp_map_para.lines = 0;
+         tp_map_para.lines < 20; tp_map_para.lines++)
+    {
+        for (tp_map_para.cols = 0;
+             tp_map_para.cols < 20; tp_map_para.cols++)
+        {
+
+            putchar (tp_map_para.border[tp_map_para.lines][tp_map_para.cols]);
+
+        }
+        putchar (10);
+
+    }    
     system("pause");
     return 0;
 }
+****************************************************/
+
+
+
